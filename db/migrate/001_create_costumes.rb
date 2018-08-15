7511238e6e21624b6261032b8bb4218bdf33e72b
Wasif Khan <waasif.khaan@gmail.com>
2018-08-15 23:13:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.float :price
       t.text  :size 
       t.string :image_url
-      t.timestamp :datetime 
+      t.string :timestamp 
     end 
   end 
 end 
