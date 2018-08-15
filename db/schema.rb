957be41c3316f1a531e0ac5adbf518994ec33e58
Wasif Khan <waasif.khaan@gmail.com>
2018-08-15 23:22:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.text "name"
     t.float "price"
     t.text "size"
-    t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "img_url"
   end
 
   create_table "houses", force: :cascade do |t|
