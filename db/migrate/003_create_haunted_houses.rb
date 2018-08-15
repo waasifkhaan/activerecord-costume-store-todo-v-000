18990c3ec30b63565fbd4a11e41c18386fe5162b
Wasif Khan <waasif.khaan@gmail.com>
2018-08-15 23:21:32 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change 
     create_table :houses do |t|
       t.text :name 
+      t.string :location 
+      t.text :theme
       t.float :price
+      t.boolean :family_friendly
+      t.timestamp :opening_time
+      t.timestamp :closing_time
       t.text  :size 
       t.string :image_url
       t.timestamp :created_at
